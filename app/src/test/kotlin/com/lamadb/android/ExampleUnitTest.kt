ae97b7c572e94fd67461483c945fb1aa16dab2cb
Ali Forfaen <ali@illamanati.com>
2026-07-05 17:58:53 +0200

    test: add example JVM unit test

---
+++ app/src/test/kotlin/com/lamadb/android/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.lamadb.android
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun appName_isCorrect() {
+        assertEquals("LamaDB", "LamaDB")
+    }
+}
